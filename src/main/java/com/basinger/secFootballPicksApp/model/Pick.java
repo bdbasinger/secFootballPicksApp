33f7049eddfcd92a492297971e4dd4d3779d404b
Brennan Basinger <brennan.d.basinger@gmail.com>
2023-09-04 10:25:45 -0500

    Added Game and Pick model classes

--- src/main/java/com/basinger/secFootballPicksApp/model/Pick.java
+++ src/main/java/com/basinger/secFootballPicksApp/model/Pick.java
@@ -1,26 +1,29 @@
 package com.basinger.secFootballPicksApp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-@Entity(name = "_UserPicks")
-@Getter
-@Setter
+@Entity
+@Data
+@Table(name="picks")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Pick {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String team;
-    private String opponent;
-    private Integer week;
-    private String result;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    private String prediction; // "W" or "L"
 
 }
