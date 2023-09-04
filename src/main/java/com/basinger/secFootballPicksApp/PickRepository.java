133fb3703c1f43323566f50bb110c0d5bcfe785d
Brennan Basinger <brennan.d.basinger@gmail.com>
2023-09-04 09:19:37 -0500

    User Registration implemented

--- src/main/java/com/basinger/secFootballPicksApp/PickRepository.java
+++ src/main/java/com/basinger/secFootballPicksApp/PickRepository.java
@@ -4,4 +4,5 @@ import com.basinger.secFootballPicksApp.model.Pick;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PickRepository extends JpaRepository<Pick, Long> {
+
 }
